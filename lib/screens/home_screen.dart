a719a8b55444cdfc30efe7583f3f1979916670ad
Minhee Seo <hwarakhada@gmail.com>
2022-05-20 10:30:03 +0900

    위젯 분리

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,19 +10,65 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   // latitude - 위도, longitude - 경도
-  static final LatLng companyLatLng = LatLng(37.5233273, 126.921251);
+  static final LatLng companyLatLng = LatLng(37.501520, 126.787560);
+
+  // CameraPosition : 우주에서 지구를 바라보는 시점
   static final CameraPosition initialPosition = CameraPosition(
-    target: companyLatLng,
-    zoom: 15,
+    target: companyLatLng, // target엔 위도와 경도를 넣어주면 됨
+    zoom: 15, // 작을수록 멀리! 클수록 가깝게
   );
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: GoogleMap(
-          initialCameraPosition: initialPosition,
+        appBar: renderAppBar(),
+        body: Column(
+          children: [
+            _CustomGoogleMap(initialPosition: initialPosition),
+            _ChoolCheckButton(),
+          ],
         ),
       ),
+    );
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        '오늘도 출근',
+        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w700),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+  const _CustomGoogleMap({Key? key, required this.initialPosition})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        initialCameraPosition:
+            initialPosition, // 구글 지도를 처음 실행했을때 어떤 위치를 바라보고 있을지
+        mapType: MapType.normal, // 높낮이가 표시가 됨
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text('출근'),
     );
   }
 }
